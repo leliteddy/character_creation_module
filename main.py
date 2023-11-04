a7950eb247abee6e5a27d7a74b3e107e655daef4
Стас Басов <stas.basov@whatever.com>
2023-11-04 15:25:33 +0500

    Class update

--- main.py
+++ main.py
@@ -1,21 +1,93 @@
 from random import randint
+DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80  
+
 
 class Character:
-    def __init__(self, name, class_type):
+    # Новая константа.
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    RANGE_VALUE_ATTACK = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
+    SPECIAL_BUFF = 15
+    SPECIAL_SKILL = 'Удача'
+    ...
+if char_class == 'warrior':
+    print('Воитель — дерзкий воин ближнего боя. '
+          'Сильный, выносливый и отважный.')
+if char_class == 'mage':
+    print('Маг — находчивый воин дальнего боя. '
+          'Обладает высоким интеллектом.')
+if char_class == 'healer':
+    print('Лекарь — могущественный заклинатель. '
+          'Черпает силы из природы, веры и духов.')
+    
+    ...
+
+warrior = Warrior('Кодослав')
+print(warrior)
+print(warrior.attack())
+
+# Вывод в терминал:
+# Warrior — дерзкий воин ближнего боя. Сильный, выносливый и отважный.
+# Кодослав нанёс урон противнику, равный 8
+
+
+...
+
+
+    def __init__(self, name):
         self.name = name
-        self.class_type = class_type
-        self.stamina = 80
-        self.attack = 5
-        self.defense = 10
-
+    
     def attack(self):
-        pass
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}.')
+    
+    def defence(self):
+        # Вычисляем значение защиты в переменной value_defence.
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона.')
 
-    def defense(self):
-        pass
+        if char_class == 'warrior':
+            return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+        if char_class == 'mage':
+            return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+        if char_class == 'healer':
+            return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+        return '{char_name} блокировал 10 урона'
+
 
     def special(self):
-        pass
+        # Здесь описано тело метода special().
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
+
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.' 
+
+class Warrior(Character):
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
+                             'Сильный, выносливый и отважный')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+    SPECIAL_BUFF = DEFAULT_STAMINA + 25
+    SPECIAL_SKILL = 'Выносливость'
+
+class Mage(Character):
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
+                             'Обладает высоким интеллектом')
+    RANGE_VALUE_ATTACK = (5, 10)
+    RANGE_VALUE_DEFENCE = (-2, 2)
+    SPECIAL_BUFF = DEFAULT_ATTACK + 40
+    SPECIAL_SKILL = 'Атака'
+
+class Healer(Character):
+    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
+                             'Черпает силы из природы, веры и духов')
+    RANGE_VALUE_ATTACK = (-3, -1)
+    RANGE_VALUE_DEFENCE = (2, 5)
+    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
+    SPECIAL_SKILL = 'Защита' 
 
     def start_training(self):
         print(f'{self.name}, ты {self.class_type} — мастер своего дела.')
